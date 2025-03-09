a49ffc4aa8f5c4c250dc2dd0bcc61c8dcfdd03ab
Lukas Schwab <lukas.schwab@gmail.com>
2025-03-09 11:04:41 -0700

    Remove nilinterface from cmd/glint
    Use golangci-lint defaults

--- cmd/glint/glint.go
+++ cmd/glint/glint.go
@@ -3,12 +3,10 @@ package main
 import (
 	"github.com/lukasschwab/glint"
 	"github.com/lukasschwab/glint/pkg/golangci"
-	nilinterface "github.com/lukasschwab/nilinterface/pkg/analyzer"
 )
 
 func main() {
 	glint.Main(append(
 		golangci.DefaultAnalyzers(),
-		nilinterface.Analyzer,
 	)...)
 }
